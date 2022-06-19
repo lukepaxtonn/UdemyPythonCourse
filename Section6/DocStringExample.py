88fefc4cd682d5a692e145f0b4c18f211f851ec3
Luke Paxton <lxp054@student.bham.ac.uk>
2022-06-19 22:45:01 +0100

    Created Docstring Example file

---
+++ Section6/DocStringExample.py
@@ -0,0 +1,16 @@
+import random
+
+
+def randomNumberGenerator(maxNumber): 
+    """_summary_
+    
+    Args:
+        maxNumber (Int): Maximum number that can be generated
+    """
+    number = random.randrange(0, maxNumber)
+    
+    return number
+
+max = input('Please enter a maximum number: ')
+print(randomNumberGenerator(int(max)))
+help(randomNumberGenerator)
